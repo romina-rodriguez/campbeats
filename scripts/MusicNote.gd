a1d3bba9aca2d76dcd629dd6d33fddd302f15462
Feruxan <fernanda.castillo.g@ug.uchile.cl>
2022-04-05 21:48:09 -0400

    Animation and sound when collected added to script

--- scripts/MusicNote.gd
+++ scripts/MusicNote.gd
@@ -1,4 +1,9 @@
 extends Area2D
 
 func _on_MusicNote_body_entered(_body):
+	$CollectNote.play("collectedBounce")
+	$SoundCollect.play()
+	
+
+func _on_CollectNote_animation_finished(anim_name):
 	queue_free()
