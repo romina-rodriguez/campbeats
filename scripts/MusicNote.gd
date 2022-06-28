644f234e83a94391b0a893c30f3252894bbf665f
TomCalde <tomascalderon@ug.uchile.cl>
2022-06-28 17:35:41 -0400

    chenge notes music

--- scripts/MusicNote.gd
+++ scripts/MusicNote.gd
@@ -1,33 +1,25 @@
 extends Area2D
 
 var TIME = 3
-var ACTIVATE = false
+var ACTIVATE = 0
 onready var playback = $AnimationTree.get("parameters/playback")
 
 signal note_collected
 
+
 func _on_MusicNote_body_entered(body):
-	if(ACTIVATE):
-		
+	print(body.name)
+	if(ACTIVATE==0 and body.name=="Activate"):
+		ACTIVATE=1
+		playback.travel("available")
+	elif(ACTIVATE==1 and body.name=="Player"):
 		playback.travel("get")
 		emit_signal("note_collected")
 		body.add_note()
 		$SoundCollect.play()
 		
 	
-var time_start = OS.get_unix_time()
-var time_now = 0
 
-func _process(delta):
-	time_now = OS.get_unix_time()
-	var time_elapsed = time_now - time_start
-	if(TIME<time_elapsed and ACTIVATE == false and TIME!=0):
-		ACTIVATE=true
-		playback.travel("available")
-	if(time_elapsed==30 and ACTIVATE == true):
-		ACTIVATE=false
-		TIME=0
-		playback.travel("idle")
 		
 func _on_CollectNote_animation_finished(anim_name):
 	queue_free()
