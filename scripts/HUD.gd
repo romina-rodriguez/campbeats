5f345251dcb100a5fa4573037b18881ccb75b749
TomCalde <tomascalderon@ug.uchile.cl>
2022-07-13 22:34:46 -0400

    The end

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -4,12 +4,11 @@ var notes = 0
 var dash = 3
 
 func _ready():
-	if notes < 10 and dash < 10:
+	$Dash.text = "0" + String(dash)
+	if notes < 10:
 		$Notes.text = "0" + String(notes)
-		$Dash.text = "0" + String(dash)
 	else:
 		$Notes.text = String(notes)
-		$Dash.text = String(dash)
 
 func _on_MusicNote_collected():
 	notes += 1
